e6df5494f5b0b92586e6cf0cd2b193508ced1b72
Pavel <pawelv9@gmail.com>
2022-09-25 17:27:32 +0300

    minor fix

--- src/components/sign-in-form/sign-in-form-component.jsx
+++ src/components/sign-in-form/sign-in-form-component.jsx
@@ -61,7 +61,7 @@ const SignInForm = () => {
         <FormInput label='Password' type='password' required onChange={handleChange} name="password" value={password} />
         <div className='buttons-container'>
           <Button type="submit">Sign In</Button>
-          <Button buttonType='google' onClick={signInWithGoogle}>Google</Button>
+          <Button buttonType='google' onClick={signInWithGoogle}>Google In</Button>
         </div>
       </form>
     </div>
